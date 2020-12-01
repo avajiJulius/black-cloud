928479913b9e0b5dc7408efe3125f06a76283967
Alexandr Matushkin <avajijulius@icloud.com>
2020-12-01 18:13:56 +0300

    Delete Desktop module

--- desktop-app/src/main/java/com/blackcloud/desktop/service/zip/IZipService.java
+++
@@ -1,9 +0,0 @@
-package com.blackcloud.desktop.service.zip;
-
-import java.io.IOException;
-import java.nio.file.Path;
-
-public interface IZipService {
-    String zip(Path srcPath) throws IOException;
-    void unzip(Path srcPath) throws IOException;
-}
